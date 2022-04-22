94e14a52b3b9b7d6182188b4fa4601c42a3eb5cc
AliceOneLove <syava19999@gmail.com>
2022-04-22 16:22:29 +0500

    ...

---
+++ index.js
@@ -0,0 +1,13 @@
+function checkInputs() {
+    let elements = ['name', 'last-name', 'email', 'password'];
+    for (e of elements) {
+        let input = document.getElementById(e);
+        if (input.value == '') {
+            input.setAttribute("style", "background: url(images/icon-error.svg) no-repeat 95%;"
+            + "outline: 1px solid hsl(0, 100%, 74%);");
+            if (e == 'email') {
+                input.placeholder = "email@example.com";
+            }
+        }
+    }
+}
