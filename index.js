f9a946bfee32b503809507c4340e73a66c20b7d4
AliceOneLove <syava19999@gmail.com>
2022-04-27 16:49:44 +0500

    ...

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function checkInputs() {
-    let elements = ['name', 'last-name', 'email', 'password'];
-    for (e of elements) {
-        let input = document.getElementById(e);
+    // let elements = ['inputs-block__name', 'inputs-block__last-name', 'inputs-block__email', 'inputs-block__password'];
+    let inputElements = new Map([
+        ['inputs-block__name', 'inputs-block__name-warning'],
+        ['inputs-block__last-name', 'inputs-block__last-name-warning'],
+        ['inputs-block__email', 'inputs-block__email-warning'], 
+        ['inputs-block__password', 'inputs-block__password-warning']
+    ]);
+    // let warnings = ['inputs-block__name-warning', 'inputs-block__last-name-warning', 'inputs-block__email-warning', 'inputs-block__password-warning'];
+    for (let e of inputElements.keys()) {
+        let input = document.getElementById(e),
+            warning = document.getElementById(inputElements[e]);
         if (input.value == '') {
             input.setAttribute("style", "background: url(images/icon-error.svg) no-repeat 95%;"
             + "outline: 1px solid hsl(0, 100%, 74%);");
@@ -9,6 +17,7 @@ function checkInputs() {
                 input.placeholder = "email@example.com";
                 input.classList.add('inputs-block__incorrect-email');
             }
+            warning.hidden = false;
         }
     }
 }
