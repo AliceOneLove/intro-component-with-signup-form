63a07887e86f01efb2cad43ecc7cbbcb7f847074
AliceOneLove <syava19999@gmail.com>
2022-05-04 17:25:19 +0500

    ...

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function checkInputs() {
     // let warnings = ['inputs-block__name-warning', 'inputs-block__last-name-warning', 'inputs-block__email-warning', 'inputs-block__password-warning'];
     for (let e of inputElements.keys()) {
         let input = document.getElementById(e),
-            warning = document.getElementById(inputElements[e]);
+            warning = document.getElementById(inputElements.get(e));
         if (input.value == '') {
             input.setAttribute("style", "background: url(images/icon-error.svg) no-repeat 95%;"
             + "outline: 1px solid hsl(0, 100%, 74%);");
-            if (e == 'email') {
+            if (e == 'inputs-block__email') {
                 input.placeholder = "email@example.com";
                 input.classList.add('inputs-block__incorrect-email');
             }
